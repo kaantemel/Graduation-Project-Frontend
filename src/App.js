a9a108fb18f5eed5e5c1ae7276f30092850750e2
kaantemel <72491459+kaantemel@users.noreply.github.com>
2023-06-04 22:20:40 +0300

    File Upload Added
    File Upload Added

--- src/App.js
+++ src/App.js
@@ -19,8 +19,9 @@ import {
   DirectionsRenderer,
 } from "@react-google-maps/api";
 import { useRef, useState } from "react";
+import FileUploadSingle from "./FileUploadSingle.tsx";
 
-const center = { lat: 48.8584, lng: 2.2945 };
+const center = { lat: 40.97906099583216, lng: 29.110213592605717 };
 const google = window.google;
 function App() {
   const { isLoaded } = useJsApiLoader({
@@ -30,6 +31,8 @@ function App() {
 
   const [map, setMap] = useState(/** @type google.maps.Map */ (null));
   const [directionsResponse, setDirectionsResponse] = useState(null);
+  const [directionsResponse2, setDirectionsResponse2] = useState(null);
+  const [directionsResponse3, setDirectionsResponse3] = useState(null);
   const [distance, setDistance] = useState("");
   const [duration, setDuration] = useState("");
 
@@ -46,15 +49,205 @@ function App() {
     // eslint-disable-next-line no-undef
     const directionsService = new google.maps.DirectionsService();
     const results = await directionsService.route({
-      origin: new google.maps.LatLng(40.8911872, 29.3830656),
-      destination: new google.maps.LatLng(40.9669, 29.0658),
+      origin: new google.maps.LatLng(40.97906099583216, 29.110213592605717),
+      destination: new google.maps.LatLng(
+        40.97906099583216,
+        29.110213592605717
+      ),
       waypoints: [
         {
-          location: new google.maps.LatLng(40.8207728, 29.3193615),
+          location: new google.maps.LatLng(
+            40.9341928534301,
+            29.127845358669738
+          ),
           stopover: true,
         },
         {
-          location: new google.maps.LatLng(40.967381, 29.218309),
+          location: new google.maps.LatLng(
+            40.89573973663695,
+            29.378704942949614
+          ),
+          stopover: true,
+        },
+        {
+          location: new google.maps.LatLng(
+            40.86204809981522,
+            29.297982941807657
+          ),
+          stopover: true,
+        },
+        {
+          location: new google.maps.LatLng(
+            40.922497718164124,
+            29.355389683873184
+          ),
+          stopover: true,
+        },
+        {
+          location: new google.maps.LatLng(
+            40.92276041855027,
+            29.351267181586586
+          ),
+          stopover: true,
+        },
+        {
+          location: new google.maps.LatLng(
+            40.95673365295175,
+            29.39085065571143
+          ),
+          stopover: true,
+        },
+      ],
+      // eslint-disable-next-line no-undef
+      travelMode: google.maps.TravelMode.DRIVING,
+    });
+
+    const results2 = await directionsService.route({
+      origin: new google.maps.LatLng(40.97906099583216, 29.110213592605717),
+      destination: new google.maps.LatLng(
+        40.97906099583216,
+        29.110213592605717
+      ),
+      waypoints: [
+        {
+          location: new google.maps.LatLng(
+            41.08252512422859,
+            29.02055610131978
+          ),
+          stopover: true,
+        },
+        {
+          location: new google.maps.LatLng(41.0439853329135, 29.07847544984874),
+          stopover: true,
+        },
+        {
+          location: new google.maps.LatLng(
+            41.022840229382716,
+            29.020822049275058
+          ),
+          stopover: true,
+        },
+        {
+          location: new google.maps.LatLng(
+            41.021289164720024,
+            29.044592862483224
+          ),
+          stopover: true,
+        },
+        {
+          location: new google.maps.LatLng(
+            41.04451019021357,
+            29.002037714143253
+          ),
+          stopover: true,
+        },
+        {
+          location: new google.maps.LatLng(
+            41.04962652836571,
+            29.026221256284906
+          ),
+          stopover: true,
+        },
+        {
+          location: new google.maps.LatLng(
+            41.09396491968674,
+            29.028157012506508
+          ),
+          stopover: true,
+        },
+        {
+          location: new google.maps.LatLng(
+            41.06763032881699,
+            29.008845885022897
+          ),
+          stopover: true,
+        },
+        {
+          location: new google.maps.LatLng(
+            40.959997798019515,
+            29.081336340161723
+          ),
+          stopover: true,
+        },
+        {
+          location: new google.maps.LatLng(
+            40.967516067589195,
+            29.086662503242763
+          ),
+          stopover: true,
+        },
+      ],
+      // eslint-disable-next-line no-undef
+      travelMode: google.maps.TravelMode.DRIVING,
+    });
+    const results3 = await directionsService.route({
+      origin: new google.maps.LatLng(40.97906099583216, 29.110213592605717),
+      destination: new google.maps.LatLng(
+        40.97906099583216,
+        29.110213592605717
+      ),
+      waypoints: [
+        {
+          location: new google.maps.LatLng(
+            41.00360489825688,
+            29.055025496362934
+          ),
+          stopover: true,
+        },
+        {
+          location: new google.maps.LatLng(
+            40.974003230004264,
+            29.056271501567718
+          ),
+          stopover: true,
+        },
+        {
+          location: new google.maps.LatLng(
+            40.99182393663204,
+            29.085466193310513
+          ),
+          stopover: true,
+        },
+        {
+          location: new google.maps.LatLng(
+            40.955014801132876,
+            29.12189179080731
+          ),
+          stopover: true,
+        },
+        {
+          location: new google.maps.LatLng(
+            41.09491858862192,
+            29.02799972905536
+          ),
+          stopover: true,
+        },
+        {
+          location: new google.maps.LatLng(
+            41.05665465310689,
+            29.034288669721278
+          ),
+          stopover: true,
+        },
+        {
+          location: new google.maps.LatLng(
+            40.97660923134072,
+            29.085490544024317
+          ),
+          stopover: true,
+        },
+        {
+          location: new google.maps.LatLng(
+            40.98965915843983,
+            29.119941216242307
+          ),
+          stopover: true,
+        },
+        {
+          location: new google.maps.LatLng(
+            40.94379084503687,
+            29.11738012753533
+          ),
           stopover: true,
         },
       ],
@@ -81,6 +274,8 @@ function App() {
     console.log(myDist);
     console.log(myTime);
     setDirectionsResponse(results);
+    setDirectionsResponse2(results2);
+    setDirectionsResponse3(results3);
     setDistance(distance + " km");
     setDuration(time + " min");
   }
@@ -105,7 +300,7 @@ function App() {
         {/* Google Map Box */}
         <GoogleMap
           center={center}
-          zoom={15}
+          zoom={12}
           mapContainerStyle={{ width: "100%", height: "100%" }}
           options={{
             zoomControl: false,
@@ -118,7 +313,33 @@ function App() {
           <Marker position={center} />
           {directionsResponse && (
             <>
-              <DirectionsRenderer directions={directionsResponse} />
+              <DirectionsRenderer
+                options={{
+                  markerOptions: {
+                    icon: "http://maps.google.com/mapfiles/ms/icons/blue-dot.png",
+                  },
+                  polylineOptions: { strokeColor: "blue", strokeOpacity: 0.5 },
+                }}
+                directions={directionsResponse}
+              />
+              <DirectionsRenderer
+                options={{
+                  markerOptions: {
+                    icon: "http://maps.google.com/mapfiles/ms/icons/red-dot.png",
+                  },
+                  polylineOptions: { strokeColor: "red", strokeOpacity: 0.5 },
+                }}
+                directions={directionsResponse2}
+              />
+              <DirectionsRenderer
+                options={{
+                  markerOptions: {
+                    icon: "http://maps.google.com/mapfiles/ms/icons/green-dot.png",
+                  },
+                  polylineOptions: { strokeColor: "green", strokeOpacity: 0.5 },
+                }}
+                directions={directionsResponse3}
+              />
             </>
           )}
         </GoogleMap>
@@ -129,14 +350,19 @@ function App() {
         m={4}
         bgColor="white"
         shadow="base"
-        width="30vw"
+        width="55vw"
         zIndex="1"
       >
-        <HStack spacing={2} justifyContent="space-between">
-          <ButtonGroup>
+        <HStack spacing={2} mt={4} justifyContent="space-between">
+          <ButtonGroup
+            spacing={20}
+            justifyContent="space-between"
+            style={{ width: "100vw" }}
+          >
             <Button colorScheme="pink" type="submit" onClick={calculateRoute}>
               Calculate Route
             </Button>
+            <FileUploadSingle />
             <IconButton
               aria-label="center back"
               icon={<FaTimes />}
---
+++ src/FileUploadSingle.tsx
@@ -0,0 +1,72 @@
+import { color } from "@chakra-ui/react";
+import React from "react";
+import { ChangeEvent, useState } from "react";
+
+function FileUploadSingle() {
+  const [file, setFile] = useState<File>();
+  const [fileContent, setFileContent] = useState<string>("");
+
+  const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files) {
+      setFile(e.target.files[0]);
+
+      // Read file content
+      const reader = new FileReader();
+      reader.onload = (event) => {
+        // The file's text will be printed here
+        const content = event.target?.result as string;
+
+        // Manipulate the content here
+        const manipulatedContent = content; // This is just an example. You can manipulate the content based on your needs.
+        console.log(manipulatedContent);
+        setFileContent(manipulatedContent);
+      };
+
+      reader.readAsText(e.target.files[0]);
+    }
+  };
+
+  const handleUploadClick = () => {
+    if (!file) {
+      return;
+    }
+
+    // 👇 Uploading the file using the fetch API to the server
+    fetch("https://httpbin.org/post", {
+      method: "POST",
+      body: file,
+      // 👇 Set headers manually for single file upload
+      headers: {
+        "content-type": file.type,
+        "content-length": `${file.size}`, // 👈 Headers need to be a string
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.error(err));
+  };
+
+  return (
+    <div style={{ display: "flex" }}>
+      <div>
+        <input type="file" onChange={handleFileChange} />
+        <div>{file && `${file.name} - ${file.type}`}</div>
+      </div>
+
+      <button
+        style={{
+          backgroundColor: "#B83280",
+          color: "white",
+          borderRadius: "5px",
+          marginBottom: "10px",
+          padding: "5px",
+        }}
+        onClick={handleUploadClick}
+      >
+        Upload
+      </button>
+    </div>
+  );
+}
+
+export default FileUploadSingle;
